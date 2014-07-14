d95ed953ce0491dcfbc3c2ec9bc4a342f0f7c873
JohnAZoidberg <ds@struckmeierfliesen.de>
2014-07-14 20:20:22 +0200

    BraceletFragment pictures working

--- src/net/placelet/BraceletFragment.java
+++ src/net/placelet/BraceletFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class BraceletFragment extends Fragment {
@@ -15,16 +16,23 @@ public class BraceletFragment extends Fragment {
 	private TextView ownerView;
 	private TextView picCountView;
 	private TextView lastLocationView;
+	private ImageView imgView1;
+	private ImageView imgView2;
+	private ImageView imgView3;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		braceletActivity = (BraceletActivity) getActivity();
 		bracelet = braceletActivity.bracelet;
 		View rootView = inflater.inflate(R.layout.fragment_bracelet, container, false);
+		
 		nameView = (TextView) rootView.findViewById(R.id.braceletName);
 		ownerView = (TextView) rootView.findViewById(R.id.braceletOwner);
 		picCountView = (TextView) rootView.findViewById(R.id.braceletPicCount);
 		lastLocationView = (TextView) rootView.findViewById(R.id.braceletLastLocation);
+		imgView1 = (ImageView) rootView.findViewById(R.id.imageView1);
+		imgView2 = (ImageView) rootView.findViewById(R.id.imageView2);
+		imgView3 = (ImageView) rootView.findViewById(R.id.imageView3);
 		return rootView;
 	}
 	
@@ -33,5 +41,9 @@ public class BraceletFragment extends Fragment {
 		ownerView.setText(bracelet.owner);
 		picCountView.setText(bracelet.picAnz + "");
 		lastLocationView.setText(bracelet.lastCity + ", " + bracelet.lastCountry);
+		
+		Util.loadThumbnail(braceletActivity, imgView1, bracelet.pictures.get(0).id);
+		Util.loadThumbnail(braceletActivity, imgView2, bracelet.pictures.get(1).id);
+		Util.loadThumbnail(braceletActivity, imgView3, bracelet.pictures.get(2).id);
 	}
 }
