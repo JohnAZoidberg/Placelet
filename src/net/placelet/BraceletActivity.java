197eb63195b52ba03c6c9888f6368f2fd5b1fe23
JohnAZoidberg <ds@struckmeierfliesen.de>
2014-07-14 16:32:53 +0200

    BraceletActivity Animation removed

--- src/net/placelet/BraceletActivity.java
+++ src/net/placelet/BraceletActivity.java
@@ -13,7 +13,6 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class BraceletActivity extends FragmentActivity {
 	private ViewPager mPager;
@@ -34,7 +33,6 @@ public class BraceletActivity extends FragmentActivity {
 		mPager = (ViewPager) findViewById(R.id.pager);
 		mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
 		mPager.setAdapter(mPagerAdapter);
-		mPager.setPageTransformer(true, new ZoomOutPageTransformer());
 		// initiate ActionBar
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		getActionBar().setTitle(R.string.app_name);
@@ -83,43 +81,6 @@ public class BraceletActivity extends FragmentActivity {
 		@Override
 		public int getCount() {
 			return NUM_PAGES;
-		}
-	}
-
-	private class ZoomOutPageTransformer implements ViewPager.PageTransformer {
-		private static final float MIN_SCALE = 0.85f;
-		private static final float MIN_ALPHA = 0.5f;
-
-		public void transformPage(View view, float position) {
-			int pageWidth = view.getWidth();
-			int pageHeight = view.getHeight();
-
-			if (position < -1) { // [-Infinity,-1)
-				// This page is way off-screen to the left.
-				view.setAlpha(0);
-
-			} else if (position <= 1) { // [-1,1]
-				// Modify the default slide transition to shrink the page as well
-				float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
-				float vertMargin = pageHeight * (1 - scaleFactor) / 2;
-				float horzMargin = pageWidth * (1 - scaleFactor) / 2;
-				if (position < 0) {
-					view.setTranslationX(horzMargin - vertMargin / 2);
-				} else {
-					view.setTranslationX(-horzMargin + vertMargin / 2);
-				}
-
-				// Scale the page down (between MIN_SCALE and 1)
-				view.setScaleX(scaleFactor);
-				view.setScaleY(scaleFactor);
-
-				// Fade the page relative to its size.
-				view.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE) / (1 - MIN_SCALE) * (1 - MIN_ALPHA));
-
-			} else { // (1,+Infinity]
-				// This page is way off-screen to the right.
-				view.setAlpha(0);
-			}
 		}
 	}
 }
