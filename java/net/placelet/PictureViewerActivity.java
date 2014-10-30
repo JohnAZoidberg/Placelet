b71b4c9f27779e8c37699ea0cbfb32eda0336a8e
JohnAZoidberg <ds@struckmeierfliesen.de>
2014-10-31 00:19:38 +0100

    Darker ActionBar Color

--- java/net/placelet/PictureViewerActivity.java
+++ java/net/placelet/PictureViewerActivity.java
@@ -2,6 +2,7 @@ package net.placelet;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
@@ -21,6 +22,7 @@ public class PictureViewerActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getActionBar().setBackgroundDrawable(new ColorDrawable(0xff557616));
         setContentView(R.layout.activity_picture_viewer);
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
